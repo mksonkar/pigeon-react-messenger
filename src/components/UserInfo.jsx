a5ee2a880ad1aef0b8dd5fcc8e555c6f2579907e
Mukesh Kumar <mkso13189@gmail.com>
2024-12-01 12:01:53 +0530

    Create userInfo component

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,18 @@
+import { FaEdit, FaUserCircle, FaVideo } from "react-icons/fa";
+import { FiMoreHorizontal } from "react-icons/fi";
+
+export default function UserInfo() {
+  return (
+    <div className="flex items-center gap-5">
+      <div className="justify-space-between flex items-center gap-3 p-3">
+        <FaUserCircle size={30} className="" />
+        <h2 className="font-bold">John Doe</h2>
+      </div>
+      <div className="flex gap-3">
+        <FiMoreHorizontal />
+        <FaVideo />
+        <FaEdit />
+      </div>
+    </div>
+  );
+}
