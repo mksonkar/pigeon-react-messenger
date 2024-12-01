9abce4e11a0a20343f785dd260e854097e974c84
Mukesh Kumar <mkso13189@gmail.com>
2024-12-01 10:46:04 +0530

    Add blur backdrop and other css to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="flex h-screen items-center justify-center">
+      <div className="h-[90vh] w-[90vw] rounded-3xl border border-white/10 bg-emerald-800/75 backdrop-blur-xl"></div>
+    </div>
+  );
+};
 
-export default App
+export default App;
