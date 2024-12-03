53390f142c7ad5aa9155d6f2bd7b7d6c2b1097f6
Mukesh Kumar <mkso13189@gmail.com>
2024-12-03 15:13:53 +0530

    Move layout from ChatRoom to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import ChatRoom from "./components/pages/ChatRoom";
 
 const App = () => {
-  return <ChatRoom />;
+  return (
+    <div className="flex h-screen items-center justify-center text-white">
+      <div className="bg-emerald-800/65 flex h-[90vh] w-[90vw] rounded-md border border-white/10 backdrop-blur-xl">
+        <ChatRoom />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/pages/ChatRoom.jsx
+++ src/components/pages/ChatRoom.jsx
@@ -4,16 +4,10 @@ import Details from "../Details";
 
 export default function ChatRoom() {
   return (
-    <div className="flex h-screen items-center justify-center text-white">
-      <div className="flex h-[90vh] w-[90vw] justify-start rounded-md border border-white/10 bg-emerald-800/75 backdrop-blur-xl">
-        <Sidebar />
-        <div className="">
-          <Chat />
-        </div>
-        <div>
-          <Details />
-        </div>
-      </div>
-    </div>
+    <>
+      <Sidebar />
+      <Chat />
+      <Details />
+    </>
   );
 }
